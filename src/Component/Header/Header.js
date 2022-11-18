93b0f3f702d333978d837dd716801281bbbb1cb7
Abul Bashar <adulbashar124@gmail.com>
2022-11-18 03:51:52 +0000

    install react-router

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import logo from '../../images/Logo.svg'
-import './Header.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../images/Logo.svg";
+import "./Header.css";
 const Header = () => {
-    return (
-        <nav className='header'>
-            <img src={logo} alt=""/>
-            <div>
-            <a href="order">Order</a>
-            <a href="/review">Order Review</a>
-            <a href="/inventory">Manage Inventory</a>
-            <a href="/login">Login</a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="header">
+      <Link to="/home">
+        <img src={logo} alt="" />
+      </Link>
+      <div>
+        <Link to="order">Order</Link>
+        <Link to="/review">Order Review</Link>
+        <Link to="/inventory">Manage Inventory</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
