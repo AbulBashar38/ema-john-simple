e38e50e65ce7b013cf74b09b2c9c78c1c8b565dc
Abul Bashar <adulbashar124@gmail.com>
2022-11-18 06:58:09 +0000

    cart is updating quantity when same item is add more times

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -1,30 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import Product from '../Product/Product';
-import './Shop.css'
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
+import "./Shop.css";
 const Shop = () => {
-    const[cart,setCart]=useState([])
-    const [products,setProducts]=useState([])
-    useEffect(()=>{
-        fetch('products.json')
-        .then(res=>res.json())
-        .then(data=>setProducts(data))
-    },[])
-    const handleAddToCart =(product)=>{
-        const newCart =[...cart,product]
-        setCart(newCart)
+  const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  const handleAddToCart = (ClickedProduct) => {
+    if (cart.length > 0) {
+      cart.find((cartProduct) => {
+        if (cartProduct.id === ClickedProduct.id) {
+          const updateQuantity = ++cartProduct.quantity;
+          ClickedProduct["quantity"] = updateQuantity;
+          const notSameProduct = cart.filter(
+            (product) => product.id !== ClickedProduct.id
+          );
+          setCart([...notSameProduct, ClickedProduct]);
+        } else {
+          ClickedProduct["quantity"] = 1;
+          const newCart = [...cart, ClickedProduct];
+          setCart(newCart);
+        }
+      });
+    } else {
+      ClickedProduct["quantity"] = 1;
+      setCart([ClickedProduct]);
     }
-    return (
-        <div className='shop-container'>
-            <div className="product-container">
-                {
-                    products.map(product=><Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>)
-                }
-            </div>
-            <div className="cart-container">
-                <h1>hello</h1>
-            </div>
-        </div>
-    );
+    console.log(cart);
+  };
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="cart-container"></div>
+    </div>
+  );
 };
 
 export default Shop;
